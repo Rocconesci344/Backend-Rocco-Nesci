7c9f0182bd5b73720fc3a296ac5ccd29b04bb2a0
rocconesci344 <rocconesci344@gmail.com>
2024-03-03 13:03:54 -0300

    Arreglando errores

--- public/scripts/realTimeProducts.js
+++ public/scripts/realTimeProducts.js
@@ -2,25 +2,25 @@ import io from '/socket.io/socket.io.js';
 
 const socket = io();
 
-// Escuchar eventos de productos agregados y eliminados y actualizar la lista en la vista
 socket.on('productAdded', (product) => {
     const productList = document.getElementById('productList');
     const listItem = document.createElement('li');
-    listItem.textContent = product;
+    listItem.textContent = `Title: ${product.title}, Description: ${product.description}, Price: ${product.price}`;
     productList.appendChild(listItem);
 });
 
-socket.on('productDeleted', (product) => {
+socket.on('productDeleted', (productId) => {
     const productList = document.getElementById('productList');
     const items = productList.getElementsByTagName('li');
     Array.from(items).forEach((item) => {
-        if (item.textContent === product) {
+        const itemId = item.dataset.productId;
+        if (itemId === productId) {
             productList.removeChild(item);
         }
     });
 });
 
-// Manejar el envío del formulario para agregar un producto
+
 document.getElementById('productForm').addEventListener('submit', (event) => {
     event.preventDefault();
     const productName = document.getElementById('productName').value;
@@ -28,7 +28,6 @@ document.getElementById('productForm').addEventListener('submit', (event) => {
     document.getElementById('productName').value = '';
 });
 
-// Manejar el envío del formulario para eliminar un producto
 document.getElementById('deleteForm').addEventListener('submit', (event) => {
     event.preventDefault();
     const productIndex = document.getElementById('productIndex').value;
